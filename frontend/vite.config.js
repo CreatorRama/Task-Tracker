be70cfec567571d0c8610b5b843323efedff65d5
Creator Rama <ap410485@gmail.com>
2025-05-09 21:53:33 +0530

    changed backend url link for deployment in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://task-tracker-cee5.onrender.com',
         changeOrigin: true,
         secure: false,
       },
